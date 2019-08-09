5d878ce3a65ef937dbe17ff69f1c9fc730168ac4
Hemant Singh <hemant.singh@nagarro.com>
2019-08-09 16:21:13 +0530

    build

---
+++ build/precache-manifest.82d1df48a4c68e07356686a68a0bca2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266a2cf0b0ef9261eb13b6d19d728e0a",
+    "url": "/News-React/index.html"
+  },
+  {
+    "revision": "4c564739380e789c8bba",
+    "url": "/News-React/static/css/2.0ba87e64.chunk.css"
+  },
+  {
+    "revision": "7ec191042943c340e5e3",
+    "url": "/News-React/static/css/main.f0470f58.chunk.css"
+  },
+  {
+    "revision": "4c564739380e789c8bba",
+    "url": "/News-React/static/js/2.df7e85f1.chunk.js"
+  },
+  {
+    "revision": "7ec191042943c340e5e3",
+    "url": "/News-React/static/js/main.94ffdb2d.chunk.js"
+  },
+  {
+    "revision": "ac03c205bd7ed10d6c14",
+    "url": "/News-React/static/js/runtime~main.7d41ec62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/News-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
