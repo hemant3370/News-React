774aa143c3eda622c65bdcb17759f0d1e506e2f2
Hemant Singh <hemant.singh@nagarro.com>
2019-08-09 15:32:28 +0530

    build

---
+++ build/precache-manifest.1b193199e4f02a239161359c7da0cff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4dbe15928523d10d2fea5435c35b0e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cf91dd791d396a74087",
+    "url": "/static/css/2.0ba87e64.chunk.css"
+  },
+  {
+    "revision": "145b490d8df2c3fe9079",
+    "url": "/static/css/main.f0470f58.chunk.css"
+  },
+  {
+    "revision": "0cf91dd791d396a74087",
+    "url": "/static/js/2.2279f2c3.chunk.js"
+  },
+  {
+    "revision": "145b490d8df2c3fe9079",
+    "url": "/static/js/main.f829790a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
